4107e8c4110590925fbaff20b089c769e3c3575f
Taras Tikan <109157753+TarasTikan@users.noreply.github.com>
2025-08-24 23:48:53 +0300

    add func femov menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 const form = document.querySelector(".form-container");
 const listMenu = document.querySelector(".list-menu");
+
+const updateMenu = () => {
+  let menu = JSON.parse(localStorage.getItem("menu"));
+  if (!menu) {
+    menu = [];
+  }
+  listMenu.innerHTML = "";
+  listMenu.insertAdjacentHTML(
+    "afterbegin",
+    menu
+      .map(
+        (item, index) => `<li class="list-item" id=${index}>
+        <div><p>${item.desertName}</p>
+    
+          <form class="form-ingredients" id=${index}>
+            <label>
+              Інгредієнти:
+              <input type="text" name="ingredients" id="ingredients" required/>
+            </label>
+
+            <label>
+              Кількість г/ш в рецепті:
+              <input type="text" name="numb" id="numb" required/>
+            </label>
+            <button type="submit" class="btn-add">Додати інгредієнт</button>
+          </form>
+          </div>
+              <button type="button" class="btn-delete" data-delete="delete" id=${index}>Видалити</button>
+            </li>`
+      )
+      .join("")
+  );
+};
+
 const createMenu = (e) => {
   e.preventDefault();
   const { desert } = e.currentTarget.elements;
@@ -20,23 +54,17 @@ const createMenu = (e) => {
   desert.value = "";
 };
 
-const updateMenu = () => {
-  let menu = JSON.parse(localStorage.getItem("menu"));
-  if (!menu) {
-    menu = [];
+form.addEventListener("submit", createMenu);
+
+listMenu.addEventListener("click", (e) => {
+  if (e.target.hasAttribute("data-delete")) {
+    const id = e.target.id;
+    let menu = JSON.parse(localStorage.getItem("menu")) || [];
+    menu.splice(id, 1);
+    localStorage.setItem("menu", JSON.stringify(menu));
+    updateMenu();
   }
-  listMenu.innerHTML = "";
-  listMenu.insertAdjacentHTML(
-    "afterbegin",
-    menu
-      .map(
-        (item) => `<li class="list-item"><p>${item.desertName}</p>
-              <button type="button" class="btn-delete">Видалити</button>
-            </li>`
-      )
-      .join("")
-  );
-};
+});
+
 
 updateMenu();
-form.addEventListener("submit", createMenu);
