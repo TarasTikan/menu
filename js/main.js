126b0a6ee1e6f5ab2253803d4de60dc1dc6889da
Taras Tikan <109157753+TarasTikan@users.noreply.github.com>
2025-08-25 21:58:37 +0300

    add property

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const titleListIngredients = document.querySelector('.title-ingredients')
+// const titleListIngredients = document.querySelector('.title-ingredients')
 const form = document.querySelector(".form-container");
 const listMenu = document.querySelector(".list-menu");
-const formIngredients = document.querySelector(".form-ingredients");
-const listIngredients = document.querySelector('.list-ingredients')
+// const formIngredients = document.querySelector(".form-ingredients");
+// const listIngredients = document.querySelector('.list-ingredients')
 
 const updateMenu = () => {
   let data = JSON.parse(localStorage.getItem("menu"));
@@ -14,21 +14,33 @@ const updateMenu = () => {
             <p>${data.desertName}</p>
              </ul>
             <button type="button" class="btn-delete" data-delete="delete">Видалити десерт</button>
+              <form class="form-ingredients">
+            <label>
+              Інгредієнти:
+              <input type="text" name="ingredients" class="desert" required/>
+            </label>
+
+            <label>
+              Кількість г/ш в рецепті:
+              <input type="text" name="numb" class="desert" required/>
+            </label>
+            <button type="submit" class="btn-add">Додати інгредієнт</button>
+          </form>
           </li>`
 
   );
 
-  if (!data.recipe) return
+//   if (!data.recipe) return
 
-  listIngredients.innerHTML = "";
-  listIngredients.insertAdjacentHTML(
-    "afterbegin", data.recipe.map((item, index) => `<li class="item-ingredients"><p>${item.ingredients} <span>${item.numb}</span></p><button type="button" class="btn-delete" data-delete="delete" id=${index}>Видалити інгредієнт</button></li>`).join('')
-  );
+//   listIngredients.innerHTML = "";
+//   listIngredients.insertAdjacentHTML(
+//     "afterbegin", data.recipe.map((item, index) => `<li class="item-ingredients"><p>${item.ingredients} <span>${item.numb}</span></p><button type="button" class="btn-delete" data-delete="delete" id=${index}>Видалити інгредієнт</button></li>`).join('')
+//   );
 
-if (!data.recipe) {
-   titleListIngredients.classList.remove('hidden')
-}
-  formIngredients.classList.remove('hidden')
+// if (!data.recipe) {
+//    titleListIngredients.classList.remove('hidden')
+// }
+//   formIngredients.classList.remove('hidden')
 
 };
 
@@ -54,45 +66,50 @@ const createMenu = (e) => {
   // titleListIngredients.classList.remove('hidden')
 };
 
-const createIngredientsMenu = (e) => {
-  e.preventDefault()
-  const { ingredients, numb } = e.currentTarget.elements
-  let data = JSON.parse(localStorage.getItem("menu"));
-  if (!data) return
+// const createIngredientsMenu = (e) => {
+//   e.preventDefault()
+//   const { ingredients, numb } = e.currentTarget.elements
+//   let data = JSON.parse(localStorage.getItem("menu"));
+//   if (!data) return
 
-  data.recipe.push({
-    ingredients: ingredients.value,
-    numb: numb.value
-  })
+//   data.recipe.push({
+//     ingredients: ingredients.value,
+//     numb: numb.value
+//   })
 
-  localStorage.setItem("menu", JSON.stringify(data));
-  updateMenu();
-  ingredients.value = '';
-  numb.value = '';
-  titleListIngredients.classList.remove('hidden')
-}
+//   localStorage.setItem("menu", JSON.stringify(data));
+//   updateMenu();
+//   ingredients.value = '';
+//   numb.value = '';
+//   titleListIngredients.classList.remove('hidden')
+// }
 const menuRemove = e => {
   if (e.target.hasAttribute('data-delete')) {
     localStorage.removeItem("menu");
     listMenu.innerHTML = "";
-    listIngredients.innerHTML = "";
-    formIngredients.classList.add('hidden')
-    titleListIngredients.classList.add('hidden')
+    // listIngredients.innerHTML = "";
+    // formIngredients.classList.add('hidden')
+    // titleListIngredients.classList.add('hidden')
   }
 }
-const ingredientsRemove = e => {
-  if (e.target.hasAttribute('data-delete')) {
-    let data = JSON.parse(localStorage.getItem("menu"));
-    data.recipe.splice(e.target.id, 1)
-    localStorage.setItem("menu", JSON.stringify(data));
-    listIngredients.innerHTML = "";
-    updateMenu();
-  }
+// const ingredientsRemove = e => {
+//   if (e.target.hasAttribute('data-delete')) {
+//     let data = JSON.parse(localStorage.getItem("menu"));
+//     data.recipe.splice(e.target.id, 1)
+//     localStorage.setItem("menu", JSON.stringify(data));
+//     listIngredients.innerHTML = "";
+//     updateMenu();
+//   }
+// }
+// const formIngredients = document.querySelector(".form-ingredients");
+const propertyLisener = e => {
+  e.preventDefault();
+  console.log(e.target.elements.ingredients.value);
 }
-
-listIngredients.addEventListener('click', ingredientsRemove)
+listMenu.addEventListener("submit", propertyLisener);
+// listIngredients.addEventListener('click', ingredientsRemove)
 listMenu.addEventListener('click', menuRemove)
 form.addEventListener("submit", createMenu);
-formIngredients.addEventListener("submit", createIngredientsMenu);
+// formIngredients.addEventListener("submit", createIngredientsMenu);
 
 updateMenu();
