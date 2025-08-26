db486c6cbbd12471d62582b1af0272a2caa36a16
Taras Tikan <109157753+TarasTikan@users.noreply.github.com>
2025-08-26 15:02:00 +0000

    add

--- js/main.js
+++ js/main.js
@@ -1,88 +1,98 @@
-// const titleListIngredients = document.querySelector('.title-ingredients')
+const titleListIngredients = document.querySelector('.title-ingredients')
 const form = document.querySelector(".form-container");
 const listMenu = document.querySelector(".list-menu");
 // const formIngredients = document.querySelector(".form-ingredients");
-// const listIngredients = document.querySelector('.list-ingredients')
+const listIngredients = document.querySelector('.list-ingredients')
 
 const updateMenu = () => {
+  // console.log([] === true)
   let data = JSON.parse(localStorage.getItem("menu"));
   if (!data) return
   listMenu.innerHTML = "";
   listMenu.insertAdjacentHTML(
-    "afterbegin",
-    `   <li class="list-item">
-            <p>${data.desertName}</p>
-             </ul>
-            <button type="button" class="btn-delete" data-delete="delete">Видалити десерт</button>
-              <form class="form-ingredients">
+    "afterbegin", data.map((item, index) =>     `<li class="list-item">
+    
+            <p>${item.desertName}</p>
+            <div class="wrap-btn">
+            <button type="button" class="btn-delete" data-delete="delete" id=${index}>Видалити десерт</button>
+            <button type="button" class="btn-delete" data-add="add" id=${index}>Додати до десерту рецепт</button>
+            </div>
+            <form class="form-ingredients" id=${index}>
             <label>
-              Інгредієнти:
-              <input type="text" name="ingredients" class="desert" required/>
+              Назва рецепту:
+              <input type="text" name="nameRecipe" class="desert" required/>
             </label>
-
-            <label>
-              Кількість г/ш в рецепті:
-              <input type="text" name="numb" class="desert" required/>
-            </label>
-            <button type="submit" class="btn-add">Додати інгредієнт</button>
+            <button type="submit" class="btn-add" id=${index}>Додати рецепт</button>
           </form>
-          </li>`
-
+          </li>`).join('')
   );
+// console.log(data[0].recipeGroup.length)
+data.forEach((item, index) => {
+  console.log(`Десерт: ${item.desertName}`);
+  item.recipeGroup.forEach(recipe => {
+    console.log(`--> Рецепт: ${recipe.recipeName}`);
+  });
+});
+ listIngredients.innerHTML = ""
+  // listIngredients.insertAdjacentHTML(
+  //   "afterbegin", data.map((item, index) => `<li>
+    
+  //           <p>${item.recipeGroup[index].recipeName}</p>
 
-//   if (!data.recipe) return
-
-//   listIngredients.innerHTML = "";
-//   listIngredients.insertAdjacentHTML(
-//     "afterbegin", data.recipe.map((item, index) => `<li class="item-ingredients"><p>${item.ingredients} <span>${item.numb}</span></p><button type="button" class="btn-delete" data-delete="delete" id=${index}>Видалити інгредієнт</button></li>`).join('')
-//   );
-
-// if (!data.recipe) {
-//    titleListIngredients.classList.remove('hidden')
-// }
-//   formIngredients.classList.remove('hidden')
-
+  //         </li>`).join('')
+  // );
 };
+
 
 const createMenu = (e) => {
   e.preventDefault();
   const { desert } = e.currentTarget.elements;
-
   let data = JSON.parse(localStorage.getItem("menu"));
 
   if (!data) {
-    data = {};
+    data = [];
   }
 
-  data = {
+  const menu = {
     desertName: desert.value,
-    recipe: []
+    index:  data.length+1,
+    recipeGroup: []
   };
-
+data.push(menu)
   localStorage.setItem("menu", JSON.stringify(data));
   updateMenu();
   desert.value = "";
+  // console.log(indexMenu)
   // formIngredients.classList.remove('hidden')
   // titleListIngredients.classList.remove('hidden')
 };
 
-// const createIngredientsMenu = (e) => {
-//   e.preventDefault()
-//   const { ingredients, numb } = e.currentTarget.elements
-//   let data = JSON.parse(localStorage.getItem("menu"));
-//   if (!data) return
+const createFormMenu = (e) => {
+  if (e.target.hasAttribute('data-add')) {
+    const formIngredients = document.querySelector(".form-ingredients");
+    formIngredients.classList.remove('hidden')
+  }
+}
 
-//   data.recipe.push({
-//     ingredients: ingredients.value,
-//     numb: numb.value
-//   })
+const formReceptMenu = (e) => {
+  e.preventDefault();
+  const { nameRecipe } = e.target.elements
 
-//   localStorage.setItem("menu", JSON.stringify(data));
-//   updateMenu();
-//   ingredients.value = '';
-//   numb.value = '';
-//   titleListIngredients.classList.remove('hidden')
-// }
+  let data = JSON.parse(localStorage.getItem("menu"));
+  if (!data) return
+
+  data[e.target.id].recipeGroup.push({
+    recipeName: nameRecipe.value,
+    index:  data[e.target.id].recipeGroup.length+1,
+    recipe: []
+  })
+// console.log(data[e.target.id])
+  localStorage.setItem("menu", JSON.stringify(data));
+  updateMenu();
+  nameRecipe.value = '';
+  // titleListIngredients.classList.remove('hidden')
+}
+
 const menuRemove = e => {
   if (e.target.hasAttribute('data-delete')) {
     localStorage.removeItem("menu");
@@ -102,14 +112,38 @@ const menuRemove = e => {
 //   }
 // }
 // const formIngredients = document.querySelector(".form-ingredients");
-const propertyLisener = e => {
-  e.preventDefault();
-  console.log(e.target.elements.ingredients.value);
-}
-listMenu.addEventListener("submit", propertyLisener);
+
 // listIngredients.addEventListener('click', ingredientsRemove)
 listMenu.addEventListener('click', menuRemove)
 form.addEventListener("submit", createMenu);
-// formIngredients.addEventListener("submit", createIngredientsMenu);
+listMenu.addEventListener("submit", formReceptMenu);
+
+listMenu.addEventListener("click", createFormMenu);
 
 updateMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <form class="form-ingredients">
+    //         <label>
+    //           Інгредієнти:
+    //           <input type="text" name="ingredients" class="desert" required/>
+    //         </label>
+
+    //         <label>
+    //           Кількість г/ш в рецепті:
+    //           <input type="text" name="numb" class="desert" required/>
+    //         </label>
+    //         <button type="submit" class="btn-add">Додати інгредієнт</button>
+    //       </form>
