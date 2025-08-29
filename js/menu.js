c5ed80846772b4675eba18284ee0d465289975ad
Taras Tikan <109157753+TarasTikan@users.noreply.github.com>
2025-08-29 14:31:18 +0300

    add listMenu

--- js/menu.js
+++ js/menu.js
@@ -1,35 +1,41 @@
 const listMenu = document.querySelector(".list-menu-recepie");
 
-const updateListMenu = (menu) => {
+const updateListMenu = () => {
   const data = JSON.parse(localStorage.getItem("listMenuDesert")) || [];
+  listMenu.innerHTML = ""; // Очищаємо попередній список перед оновленням
   listMenu.insertAdjacentHTML(
     "afterbegin",
     data
       .map(
         (item) => `
-     <li>
-                <h1>${item.desertName}</h1>
-                <p>Технології рецептур ${item.desertName}</p>
-                <ul>
-                ${item.recipeGroup
-                  .map(
-                    (recipe) => `<li><h2>${recipe.recipeName}<h2><p>Список інгредієнтів</p>
-                    <ul>
-                    ${recipe.recipeIngredienst.map(
+        <li>
+          <h1>${item.desertName}</h1>
+          <p>Технології рецептур ${item.desertName}</p>
+          <ul>
+            ${item.recipeGroup
+              .map(
+                (recipe) => `
+                <li>
+                  <h2>${recipe.recipeName}</h2>
+                  <p>Список інгредієнтів</p>
+                  <ul>
+                    ${recipe.recipeIngredienst
+                      .map(
                         (ing) =>
                           `<li><p>${ing.ingredients} - ${ing.numb} г/ш</p></li>`
                       )
-                      }
-                    
-                </ul>
-                
-                </li>`
-                  )
-                  .join("")}
-            </li>
-`
+                      .join("")}
+                  </ul>
+                </li>
+              `
+              )
+              .join("")}
+          </ul>
+        </li>
+      `
       )
-    
+      .join("") // Додано об'єднання всіх десертів
   );
 };
+
 updateListMenu();
