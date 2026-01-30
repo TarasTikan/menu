2dd956845a133a2bc1cd60bb1915ff436f2db94c
Taras Tikan <109157753+TarasTikan@users.noreply.github.com>
2026-01-30 13:11:43 +0200

    add handlebars and optimize img

--- menu-project/vite.config.js
+++ menu-project/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vituum from "vituum";
 import handlebars from "@vituum/vite-plugin-handlebars";
 import postcss from "@vituum/vite-plugin-postcss";
-
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 export default defineConfig({
   plugins: [
     vituum(),
@@ -12,6 +12,9 @@ export default defineConfig({
         extname: false,
       },
     }),
-    postcss(),
+    postcss({
+      overrideBrowsersList: ["last 2 versions", "> 1%"],
+    }),
+    ViteImageOptimizer(),
   ],
 });
